a595263f38872227290e7904b08b1a1a75e0082c
demzem <yann.delemos@gmail.com>
2021-11-03 13:47:32 +0100

    Mise en place components + routing

--- src/components/TestComponent/TestComponent.tsx
+++ src/components/TestComponent/TestComponent.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
-import IngredientService from '../../services/ingredientService';
 import UserService from '../../services/userService';
 import styles from './TestComponent.module.css';
 
 const TestComponent = () => {
 
   const userService = new UserService();
-  const ingredientService = new IngredientService();
-
-  
+ 
   const register = () => {
     userService.createUser({userName: "pedro", email: "lito", password: "root"})
   }
@@ -17,30 +14,10 @@ const TestComponent = () => {
     userService.login("robert", "Secret");
   }
 
-  const createIngredient = () => {
-    ingredientService.create({name: "Courgette"}); 
-  }
-
-  const updateIngredient = () => {
-    ingredientService.update({id:8, name: "Toto"}); 
-  }
-
-  const getAllIngredient = () => {
-    ingredientService.getAll().then(response => console.log(response))
-  }
-
-  const deleteIngredient = () => {
-    ingredientService.delete(9); 
-  }
-
   return (
     <div className={styles.TestComponent}>
       <button onClick={login}>Login</button><br/>
       <button onClick={register}>Register</button><br/>
-      <button onClick={getAllIngredient}>Get all ingredients</button><br/>
-      <button onClick={createIngredient}>Create ingredient</button><br/>
-      <button onClick={updateIngredient}>Update ingredient</button><br/>
-      <button onClick={deleteIngredient}>Delete ingredient</button><br/>
     </div>
   );
 }
