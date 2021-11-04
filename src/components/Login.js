bfe9264b21824c04da8c0a5e9e24e8a4047ae60b
Farrowlia <79092276+Farrowlia@users.noreply.github.com>
2021-11-04 16:06:59 +0100

    creation du composant login

---
+++ src/components/Login.js
@@ -0,0 +1,15 @@
+import {useState} from "react";
+
+function Login() {
+    const [inputValue, setInputValue] = useState('')
+
+    return (
+        <div>
+        <div>Robert, identifie-toi !</div>
+        <div>
+            <textarea />
+        </div>
+    )
+}
+
+export default Login
